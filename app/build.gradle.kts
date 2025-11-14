49fbef8d27aceecb3043c8851ab2cab5418c79a9
seanlu2 <seanlu2@illinois.edu>
2025-11-14 01:56:27 -0600

    transcript player play button with sample audio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.google.material)
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/lingonary/TranscriptPlayerActivity.kt
@@ -0,0 +1,35 @@
+package com.example.lingonary
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class TranscriptPlayerActivity: AppCompatActivity(), View.OnClickListener {
+    lateinit var mediaPlayer: MediaPlayer
+    lateinit var playButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.transcript_player)
+        mediaPlayer = MediaPlayer.create(this, R.raw.spanish_speech)
+        playButton = findViewById(R.id.playButton)
+        playButton.setOnClickListener(this)
+    }
+    override fun onStart() {
+        super.onStart()
+    }
+    override fun onClick(v: View) {
+        if (v.id == R.id.transcript) {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.pause()
+            } else {
+                mediaPlayer.start()
+            }
+        }
+        // should make some visual indication of isPlaying
+        // Speech Attribution: Muntatge Carmen Amaya i Antonia Vilas.wav by barcelonetasonora -- https://freesound.org/s/609638/ -- License: Attribution NonCommercial 4.0
+    }
+}
